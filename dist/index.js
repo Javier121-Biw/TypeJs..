a9b4df62854385c92fe216f3be0a79992e620d08
Javier121-Biw <toontty1234@gmail.com>
2026-02-23 02:04:16 +0700

    hi

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,1 @@
 "use strict";
-console.log(1);
-setTimeout(() => { console.log(2); }, 0);
-process.nextTick(() => {
-    console.log(3);
-});
-Promise.resolve().then(() => {
-    console.log(4);
-});
-
